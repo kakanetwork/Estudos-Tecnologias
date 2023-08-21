c337fffdd682c8a9964e3f73cd194e8654da5b63
kakanetwork <Kalvimklain@gmail.com>
2023-08-21 10:29:13 -0300

    Atualização

--- Prog-JS/Funções.js
+++ Prog-JS/Funções.js
@@ -1,6 +1,25 @@
 
+// Funções 
+
+// pode utilizar a função com o console.log e sem nenhum parâmetro
 function funcao1(){
     console.log('Função ativada')
+}   
+funcao1()
+
+// da mesma forma que a função pode ter os parâmetros
+function funcao2(valor1, valor2) {
+    console.log(valor1 + valor2)
+}
+funcao2(10, 20)
+
+// outra forma de utilizar as funções com o Return 
+function funcao3(valor1, valor2, valor3) {
+    return valor1 + valor2 + valor3
 }
 
-funcao1()
+// e a saída podem ser de várias formas, desde a função direta no console
+// ou sendo atribuída a uma variável
+let retorno = funcao3(10, 20, 30)
+console.log(retorno)
+console.log(funcao3(10, 20, 30))
